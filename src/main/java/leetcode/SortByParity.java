994bfc5d7c1a357d332d930c881c889141214954
Shivani Dwivedi <shivanidwivedi@Gagans-MacBook-Pro.local>
2020-01-14 15:20:11 -0800

    Sort Array By Parity

--- src/main/java/leetcode/SortByParity.java
+++ src/main/java/leetcode/SortByParity.java
@@ -1,4 +1,26 @@
 package leetcode;
 
+/**
+ * Given an array arr of non-negative integers, return an array consisting of all the even elements of arr,
+ * followed by all the odd elements of arr.
+ */
 public class SortByParity {
+    public static int[] sortByParity(int[] arr){
+        if (arr.length==0||arr.length==1){
+            return arr;
+        }
+        int result[] = new int[arr.length];
+        int index=0;
+        for(int i = 0; i < arr.length; i++){
+            if (arr[i] % 2 == 0){
+                result[index++] = arr[i];
+            }
+        }
+        for(int i = 0; i < arr.length; i++){
+            if (arr[i] % 2 != 0){
+                result[index++] = arr[i];
+            }
+        }
+        return result;
+    }
 }
--- src/test/java/leetcode/SortByParityTest.java
+++ src/test/java/leetcode/SortByParityTest.java
@@ -1,4 +1,16 @@
 package leetcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
 public class SortByParityTest {
+
+    @Test
+    public void sortByParity_test(){
+        Assert.assertEquals(Arrays.toString(SortByParity.sortByParity(new int[]{})), Arrays.toString(new int[]{}));
+        Assert.assertEquals(Arrays.toString(SortByParity.sortByParity(new int[]{5})), Arrays.toString(new int[]{5}));
+        Assert.assertEquals(Arrays.toString(SortByParity.sortByParity(new int[]{3,1,2,4})), Arrays.toString(new int[]{2,4,3,1}));
+    }
 }
