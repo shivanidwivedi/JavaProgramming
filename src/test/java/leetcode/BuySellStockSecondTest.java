24f229094c2920d1e1077956630692f8fb4cbf66
Shivani Dwivedi <shivanidwivedi@Gagans-MacBook-Pro.local>
2020-01-21 19:58:26 -0800

    Best Time to Buy and Sell Stock II

---
+++ src/test/java/leetcode/BuySellStockSecondTest.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author shivanidwivedi on 21/01/20
+ * @project JavaProgramming
+ */
+public class BuySellStockSecondTest {
+
+    @Test
+    public void maxProfit_test(){
+        Assert.assertEquals(7, BuySellStockSecond.maxProfit(new int[]{7,1,5,3,6,4}));
+        Assert.assertEquals(4, BuySellStockSecond.maxProfit(new int[]{1,2,3,4,5}));
+        Assert.assertEquals(0, BuySellStockSecond.maxProfit(new int[]{7,6,4,3,1}));
+    }
+}
