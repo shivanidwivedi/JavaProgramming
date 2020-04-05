6d69c629893b9c2fe86b217fb3d261deaa8ac472
Shivani Dwivedi <shivanidwivedi@Gagans-MacBook-Pro.local>
2020-04-05 11:11:14 -0700

    buy and sell stock II

--- src/main/java/leetcode/BuySellStockSecond.java
+++ src/main/java/leetcode/BuySellStockSecond.java
@@ -26,4 +26,13 @@ public class BuySellStockSecond {
         }
         return maxProfit;
     }
+    public static int maxProfit2(int[] prices){
+        int maxprofit = 0;
+        for (int i = 1; i < prices.length; i++){
+            if (prices[i] > prices[i - 1]){
+                maxprofit += prices[i] - prices[i-1];
+            }
+        }
+        return maxprofit;
+    }
 }
--- src/test/java/leetcode/BuySellStockSecondTest.java
+++ src/test/java/leetcode/BuySellStockSecondTest.java
@@ -15,4 +15,10 @@ public class BuySellStockSecondTest {
         Assert.assertEquals(4, BuySellStockSecond.maxProfit(new int[]{1,2,3,4,5}));
         Assert.assertEquals(0, BuySellStockSecond.maxProfit(new int[]{7,6,4,3,1}));
     }
+    @Test
+    public void maxProfit_tes2t(){
+        Assert.assertEquals(7, BuySellStockSecond.maxProfit2(new int[]{7,1,5,3,6,4}));
+        Assert.assertEquals(4, BuySellStockSecond.maxProfit2(new int[]{1,2,3,4,5}));
+        Assert.assertEquals(0, BuySellStockSecond.maxProfit2(new int[]{7,6,4,3,1}));
+    }
 }
