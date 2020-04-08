31910023a46957070f9bea6cdb6ddac5dc1e76f8
Shivani Dwivedi <shivanidwivedi@Gagans-MacBook-Pro.local>
2020-04-08 11:13:14 -0700

    Trie implementation (insert)

--- src/main/java/dataStructure/trie/TrieImplementation.java
+++ src/main/java/dataStructure/trie/TrieImplementation.java
@@ -1,8 +1,53 @@
 package dataStructure.trie;
 
+import leetcode.Trees.TreeNode;
+
 /**
  * @author shivanidwivedi on 06/04/20
  * @project JavaProgramming
+ * TRIE IMPLEMENTATION
+ * TC: O(l*n) l=average length of words n= total no of words
  */
 public class TrieImplementation {
+    private static TrieNode root;
+    public TrieImplementation(){
+        root = new TrieNode();
+    }
+    /**
+    Insert into trie - iterative
+     **/
+    public static void insert(String word){
+        TrieNode current = root;
+        for (int i = 0; i < word.length(); i++){
+            char ch = word.charAt(i);
+            TrieNode node = current.children.get(ch);
+            if (current == null){
+                node = new TrieNode();
+                current.children.put(ch,node);
+            }
+            current = node;
+        }
+        current.endOfWord = true;
+    }
+
+
+    /**
+     Insert into trie - Recursive
+     **/
+    public static void insertRecursively(String word){
+        insertRecursively(root,word,0);
+    }
+    private static void insertRecursively(TrieNode current, String word, int index){
+        if (index == word.length()){
+            current.endOfWord = true;
+            return;
+        }
+        char ch = word.charAt(index);
+        TrieNode node = current.children.get(ch);
+        if (current == null){
+            node = new TrieNode();
+            current.children.put(ch,node);
+        }
+        insertRecursively(current,word,index + 1);
+    }
 }
--- src/main/java/dataStructure/trie/TrieNode.java
+++ src/main/java/dataStructure/trie/TrieNode.java
@@ -1,8 +1,17 @@
 package dataStructure.trie;
 
+import java.util.HashMap;
+
 /**
  * @author shivanidwivedi on 06/04/20
  * @project JavaProgramming
  */
 public class TrieNode {
+    HashMap<Character, TrieNode> children;
+    boolean endOfWord;
+
+     TrieNode() {
+        this.children = new HashMap<>();
+        this.endOfWord = false;
+    }
 }
