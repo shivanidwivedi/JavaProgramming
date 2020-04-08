0bcbfab9a3d788f3dd9c3ca1f9ca7a38c334ea00
Shivani Dwivedi <shivanidwivedi@Gagans-MacBook-Pro.local>
2020-04-08 11:13:55 -0700

    K Closest points to origin

--- src/main/java/leetcode/KClosestPointsToOrigin.java
+++ src/main/java/leetcode/KClosestPointsToOrigin.java
@@ -1,8 +1,30 @@
 package leetcode;
 
+import java.util.Arrays;
+
 /**
  * @author shivanidwivedi on 07/04/20
  * @project JavaProgramming
  */
 public class KClosestPointsToOrigin {
+    public static int[][] kClosest(int[][] points, int K) {
+        int [] dist = new int[points.length];
+        for (int i = 0; i < points.length; i++){
+            dist[i] = distance(points[i]);
+        }
+        Arrays.sort(dist);
+        int disK = dist[K-1];
+        int result[][] = new int[K][2];
+        int t = 0;
+        for (int i = 0; i < points.length; i++){
+            if (distance(points[i]) <= disK){
+                result [t] = points[i];
+                t++;
+            }
+        }
+        return result;
+    }
+    private static int distance(int [] point){
+        return point[0] * point[0] + point[1] * point[1];
+    }
 }
--- src/test/java/leetcode/KClosestPointsToOriginTest.java
+++ src/test/java/leetcode/KClosestPointsToOriginTest.java
@@ -1,3 +1,10 @@
+package leetcode;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 /**
@@ -5,5 +12,11 @@ import static org.junit.Assert.*;
  * @project JavaProgramming
  */
 public class KClosestPointsToOriginTest {
-
+    @Test
+    public void kClosest_test(){
+        Assert.assertEquals(new int[][]{{-2, 2}}, KClosestPointsToOrigin.kClosest(new int[][]
+                {{1,3},{-2,2}},1));
+        Assert.assertEquals(new int[][]{{3, 3},{-2,4}}, KClosestPointsToOrigin.kClosest(new int[][]
+                {{3,3},{5,-1},{-2,4}},2));
+    }
 }
