9386be3d49fb96d2f228fca1147be4e08692d4e2
Shivani Dwivedi <shivanidwivedi@Gagans-MacBook-Pro.local>
2020-04-11 17:12:22 -0700

    Compare version numbers

---
+++ src/test/java/leetcode/CompareVersionNumbersTest.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author shivanidwivedi on 11/04/20
+ * @project JavaProgramming
+ */
+public class CompareVersionNumbersTest {
+    @Test
+    public void compareVersion_test(){
+        Assert.assertEquals(-1, CompareVersionNumbers.compareVersion("0.1","0.1.2.3.3"));
+        Assert.assertEquals(1, CompareVersionNumbers.compareVersion("1.0.1","1"));
+        Assert.assertEquals(-1, CompareVersionNumbers.compareVersion("7.5.2.4","7.5.3"));
+        Assert.assertEquals(0, CompareVersionNumbers.compareVersion("1.01","1.001"));
+        Assert.assertEquals(0, CompareVersionNumbers.compareVersion("1.0","1.0.0"));
+    }
+}
