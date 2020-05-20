3d21ba01d6975aa954a176c36549a7dae66ddabc
Shivani <shivanidwivedi168@gmail.com>
2020-05-20 10:52:40 -0700

    Max vowels substring

--- src/main/java/leetcode/Strings/MaxVowelsSubstring.java
+++ src/main/java/leetcode/Strings/MaxVowelsSubstring.java
@@ -5,4 +5,37 @@ package leetcode.Strings;
  * @project JavaProgramming
  */
 public class MaxVowelsSubstring {
+    public static String maxVowels(String string, int k){
+        if (string.length() < k){
+            return "";
+        }
+        String result = "";
+        int maxVowels = 0, first = 0, second = k - 1;
+        int index1 = 0, index2 = k;
+      while(first < string.length() && second < string.length()){
+          int currVowels = 0;
+          for(int i = first; i <= second ; i++){
+              if (isVowel(string.charAt(i))){
+                  currVowels++;
+              }
+          }
+
+          if (currVowels > maxVowels){
+
+              result =  string.substring(first,second + 1);
+          }
+
+          maxVowels = Math.max(maxVowels,currVowels);
+          first++;
+          second++;
+      }
+        return result;
+    }
+
+    private static boolean isVowel(char ch){
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+            return true;
+        }
+        return false;
+    }
 }
--- src/test/java/leetcode/Strings/MaxVowelsSubstringTest.java
+++ src/test/java/leetcode/Strings/MaxVowelsSubstringTest.java
@@ -1,9 +1,25 @@
+package leetcode.Strings;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 /**
  * @author shivanidwivedi on 24/04/20
  * @project JavaProgramming
  */
-public class MaxVowelsSubstringTest {
+class MaxVowelsSubstringTest {
 
+    @Test
+    public void maxVowels() {
+        Assert.assertEquals("gio", MaxVowelsSubstring.maxVowels("ncdfgion",3));
+        Assert.assertEquals("eufgi", MaxVowelsSubstring.maxVowels("ncdeufgion",5));
+        Assert.assertEquals("aa", MaxVowelsSubstring.maxVowels("naacdfgion",2));
+        Assert.assertEquals("eiuou", MaxVowelsSubstring.maxVowels("eiuou",5));
+        Assert.assertEquals("", MaxVowelsSubstring.maxVowels("n",3));
+    }
 }
