400cd7fd4a72df7360b536f58b18b712682fbdc2
Shivani <shivanidwivedi168@gmail.com>
2020-06-05 07:16:50 -0700

    Thread unsafe example

---
+++ src/main/java/Threads/DemoThreadUnsafe.java
@@ -0,0 +1,79 @@
+package Threads;
+
+/**
+ * @author shivanidwivedi on 05/06/20
+ * @project JavaProgramming
+ */
+import java.util.Random;
+
+class DemoThreadUnsafe {
+
+    // We'll use this to randomly sleep our threads
+    static Random random = new Random(System.currentTimeMillis());
+
+    public static void main(String args[]) throws InterruptedException {
+
+        // create object of unsafe counter
+        ThreadUnsafeCounter badCounter = new ThreadUnsafeCounter();
+
+        // setup thread1 to increment the badCounter 200 times
+        Thread thread1 = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                for (int i = 0; i < 100; i++) {
+                    badCounter.increment();
+                    DemoThreadUnsafe.sleepRandomlyForLessThan10Secs();
+                }
+            }
+        });
+
+        // setup thread2 to decrement the badCounter 200 times
+        Thread thread2 = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                for (int i = 0; i < 100; i++) {
+                    badCounter.decrement();
+                    DemoThreadUnsafe.sleepRandomlyForLessThan10Secs();
+                }
+            }
+        });
+
+        // run both threads
+        thread1.start();
+        thread2.start();
+
+        // wait for t1 and t2 to complete.
+        thread1.join();
+        thread2.join();
+
+        // print final value of counter
+        badCounter.printFinalCounterValue();
+    }
+
+    public static void sleepRandomlyForLessThan10Secs() {
+        try {
+            Thread.sleep(random.nextInt(10));
+        } catch (InterruptedException ie) {
+        }
+    }
+}
+
+class ThreadUnsafeCounter {
+
+    int count = 0;
+
+    public void increment() {
+        count++;
+    }
+
+    public void decrement() {
+        count--;
+    }
+
+    void printFinalCounterValue() {
+        System.out.println("counter is: " + count);
+    }
+}
+
