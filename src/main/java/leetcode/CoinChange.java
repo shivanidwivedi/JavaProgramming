12a554200328f02de5a2bfc230dd551eb35c76f2
Shivani <shivanidwivedi168@gmail.com>
2020-06-29 10:48:05 -0700

    Coin change problem dynamic programming

--- src/main/java/leetcode/CoinChange.java
+++ src/main/java/leetcode/CoinChange.java
@@ -6,7 +6,9 @@ import java.util.Arrays;
  * @author shivanidwivedi on 07/04/20
  * @project JavaProgramming
  *
- * You are given coins of different denominations and a total amount of money amount. Write a function to compute the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+ * You are given coins of different denominations and a total amount of money amount.
+ * Write a function to compute the fewest number of coins that you need to make up that amount.
+ * If that amount of money cannot be made up by any combination of the coins, return -1.
  *
  * Example 1:
  *
@@ -21,20 +23,38 @@ import java.util.Arrays;
 public class CoinChange {
 
     /**
-     * O(nLogn) - sorting method
+     * TC: O(amount * coins)
+     * SC: O(amount)
      * @param coins
      * @param amount
      * @return
      */
     public static int coinChangeBySort(int[] coins, int amount) {
-        int numberOfCoins = 0;
-        Arrays.sort(coins);
-        for (int i = coins.length - 1; i >= 0; i--){
-            if (coins[i] <= amount){
-                numberOfCoins += amount/coins[i];
-                amount = amount % coins[i];
+        int T[] = new int[amount + 1];
+        int R[] = new int[amount + 1];
+        T[0] = 0;
+        for(int i = 1; i<= amount; i++){
+            T[i] = Integer.MAX_VALUE - 1;
+            R[i] = -1;
+        }
+        for(int j = 0; j < coins.length; j++){
+            for(int i = 1; i <= amount; i++){
+                if(i >= coins[j]){
+                    T[i] = Math.min(T[i], 1 + T[i - coins[j]]);
+                    R[i] = j;
+                }
             }
         }
-        return (amount == 0) ? numberOfCoins : -1;
+        printCoin(R,coins);
+        return T[amount] > amount ? -1 : T[amount];
+    }
+
+    private static void printCoin(int[] R, int[] coins){
+        if(R[R.length-1] == -1) return;
+        int start = R.length - 1;
+        while (start != 0){
+            int j = R[start];
+            start -= coins[j];
+        }
     }
 }
