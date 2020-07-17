e224215bf6cdb05abe3338d655fe11ce8250b7b4
Shivani <shivanidwivedi168@gmail.com>
2020-07-17 15:36:05 -0700

    Set matrix zeroes

--- src/main/java/leetcode/SetMatrixZeroes.java
+++ src/main/java/leetcode/SetMatrixZeroes.java
@@ -45,4 +45,23 @@ public class SetMatrixZeroes {
             }
         }
     }
+//More optimized solution
+    public void setZeroes2(int[][] matrix) {
+        boolean[] row = new boolean[matrix.length];
+        boolean[] col = new boolean[matrix[0].length];
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[0].length; j++){
+                if(matrix[i][j] == 0){
+                    row[i] = col[j] = true;
+                }
+            }
+        }
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[0].length; j++){
+                if(row[i] || col[j]){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
 }
