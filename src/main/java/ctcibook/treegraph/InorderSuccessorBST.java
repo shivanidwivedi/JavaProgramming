81c521c929f6c765d9ef71e6b12442041b8a8111
Shivani <shivanidwivedi168@gmail.com>
2020-09-30 10:44:15 -0700

    Inorder successor of BST node

---
+++ src/main/java/ctcibook/treegraph/InorderSuccessorBST.java
@@ -0,0 +1,24 @@
+package ctcibook.treegraph;
+
+/**
+ * @author shivanidwivedi on 30/09/20
+ * @project JavaProgramming
+ *
+ * Write an algorithm to find the 'next'node (i.e., in-order successor) of a given node in
+ * a binary search tree. You may assume that each node has a link to its parent.
+ */
+public class InorderSuccessorBST {
+    public BinaryTreeNode inorderSuccessor(BinaryTreeNode node) {
+        if(node.right != null){
+            node = node.right;
+            while(node.left != null){
+                node = node.left;
+            }
+            return node;
+        }
+        while(node.parent != null && node == node.parent.right){
+            node = node.parent;
+        }
+        return node.parent;
+    }
+}
