3cb7786eb5320b6fa3e73e95c746ce35297e7c9f
Shivani <shivanidwivedi168@gmail.com>
2020-11-02 20:49:54 -0800

    Absolute Value sort

--- src/main/java/dataStructure/random/AbsoluteValueSort.java
+++ src/main/java/dataStructure/random/AbsoluteValueSort.java
@@ -1,8 +1,36 @@
 package dataStructure.random;
 
+import java.util.Arrays;
+
 /**
  * @author shivanidwivedi on 30/10/20
  * @project JavaProgramming
  */
 public class AbsoluteValueSort {
+    static int[] absSort(int[] nums){
+        int temp;
+
+        for (int i = 0; i < nums.length; i++) {
+            int minIndex = i;
+            for (int j = i; j < nums.length; j++) {
+                if (Math.abs(nums[j]) < Math.abs(nums[minIndex])) {
+                    minIndex = j;
+                } else if (Math.abs(nums[j]) == Math.abs(nums[minIndex])){
+                    if (nums[j] < nums[minIndex]) {
+                        minIndex = j;
+                    }
+                }
+            }
+            if (minIndex != i) {
+                temp = nums[minIndex];
+                nums[minIndex] = nums[i];
+                nums[i] = temp;
+            }
+
+        }
+        return nums;
+    }
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(AbsoluteValueSort.absSort(new int[]{2,-7,-2,-2,0})));
+    }
 }
